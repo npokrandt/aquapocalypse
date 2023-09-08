e8cfd4832873a4dafb37a5e3000e61dd3ef00180
deepwubs <pchucklambs@gmail.com>
2023-09-07 21:32:28 -0500

    added models

---
+++ models/Player.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Player extends Model {}
+
+Player.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        points: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        time_alive: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'player',
+    }
+)
+
+module.exports = Player;
